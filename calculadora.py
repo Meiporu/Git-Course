4c208ba42857e99301d13cebb3a4544df3923af8
Jose Antonio <jose.a.ferro.g@gmail.com>
2026-02-12 17:13:17 +0100

    Añadido multiplicacion y division

--- calculadora.py
+++ calculadora.py
@@ -1,25 +1,65 @@
+def leer_numeros():
+    num1 = int(input("Insertar el primer número:"))
+    num2 = int(input("Insertar el segundo número:"))
+
+    return num1, num2
+
+def suma(): 
+    num1, num2 = leer_numeros()
+
+    print("Resultado suma: ", num1 +  num2)
+
+def restar(): 
+    num1, num2 = leer_numeros()
+
+    print("Resultado resta: ", num1 -  num2)
+
+def multiplicar(): 
+    num1, num2 = leer_numeros()
+
+    print("Resultado multiplicacion: ", num1 *  num2)
+
+def dividir(): 
+    num1, num2 = leer_numeros()
+
+    if num2 == 0:
+        print("No se puede dividir entre 0 jefe")
+        return
+
+    print("Resultado division: ", num1 / num2)
+
 eleccion = -1
 
 menu = """ 
     Menú:
+    0. Salir del programa
     1. Sumar
     2. Restar
+    3. Multiplicar
+    4. Dividir
         """
 
-while eleccion < 1 or eleccion > 2:
+while eleccion != 0:
 
     print(menu)
 
     eleccion = int(input())
 
-num1 = int(input("Insertar el primer número:"))
-num2 = int(input("Insertar el segundo número:"))
+    match(eleccion): 
+        case 1: 
+            suma()
+            
+        case 2:
+            restar()
+
+        case 3:
+            multiplicar()
+
+        case 4: 
+            dividir()
+
+        case _:
+            pass 
 
 
-match(eleccion): 
-    case 1: 
-        print("Resultado suma: ", num1 +  num2)
-    case 2:
-        print("Resultado resta: ", num1 -  num2)
-    case _:
-        pass 
+    
