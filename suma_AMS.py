90a7c001b10791717c6e376bee09cdd34356508a
--global <--global>
2026-02-12 17:24:00 +0100

    Feature: add multtiplication and division

--- suma_AMS.py
+++ suma_AMS.py
@@ -1,23 +1,56 @@
 menu= """Menu de opciones  
     1. Suma 
     2. Resta
-    3. Salir\n"""
+    3. Multiplicacion 
+    4. Division
+    5. Salir\n"""
 
-op= int(input(menu))
+op = 1
 
-while op < 1 or op > 3:
-   
+while op > 1 or op < 5:
+    op= int(input(menu))
     
-    num1 = int(input("Ingrese el primer numero: "))
-    num2 = int(input("Ingrese el segundo numero: "))
+    match op:
+        case 1:
+            num1 = int(input("Ingrese el primer numero: "))
+            num2 = int(input("Ingrese el segundo numero: "))
+            resultado = num1 + num2
+            print("El resultado de la suma es: ", resultado)
+            
 
-    if op == 1:
-        resultado = num1 + num2
-        print("El resultado de la suma es: ", resultado)
-    elif op == 2:
-        resultado = num1 - num2
-        print("El resultado de la resta es: ", resultado)
-    elif op == 3:
-        print("Saliendo...")
-        exit()
+        case 2:
+            num1 = int(input("Ingrese el primer numero: "))
+            num2 = int(input("Ingrese el segundo numero: "))
+            resultado = num1 - num2
+            print("El resultado de la resta es: ", resultado)
+            
+            
+        case 3:
+            num1 = int(input("Ingrese el primer numero: "))
+            num2 = int(input("Ingrese el segundo numero: "))
+            resultado = num1 * num2
+            print("El resultado de la multiplicacion es: ", resultado)
+            
+        case 4:
+            num1 = int(input("Ingrese el primer numero: "))
+            num2 = int(input("Ingrese el segundo numero: "))
+            if(num2 == 0):
+                print("No se puede dividir por cero")
+            else:
+                resultado = num1 / num2
+                print("El resultado de la division es: ", resultado)
+            
+        case 5:
+            print("Saliendo...")
+            exit()
+        case _:
+            print("Opcion no valida, intente de nuevo")
+            
+    
+
+
+    
+
+
+   
     
